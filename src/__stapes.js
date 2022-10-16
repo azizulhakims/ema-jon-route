231ad1c591a6349ce6564b7bf168a9ea3b74b579
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-16 23:10:18 +0600

    add signup page & login page

--- src/__stapes.js
+++ src/__stapes.js
@@ -1,4 +1,13 @@
 /*
+Auth setup
+--------------------
 1. create firebase project 
+2. enable web
+3. enable sign in method
+4. install firebase
+5. get firebase config in your firebase.config.js file
+6. export app from firebase.config.js
+7. 
+
 
 */
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.css'
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>this is SignUp page</h2>
+        </div>
+    );
+};
+
+export default SignUp;
